ffc1d5a4ff52aac56513d4653437cc70691d457b
Andrey776 <61335235+Andrey776@users.noreply.github.com>
2021-04-02 22:54:13 -0600

    ongoing

--- spyder_IDE_test.py
+++ spyder_IDE_test.py
@@ -5,5 +5,25 @@ Spyder Editor
 This is a temporary script file.
 """
 import numpy as np
-a = 5
-b = np.arange(111)
+
+from sklearn.cluster import KMeans
+from sklearn import datasets
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import silhouette_score
+from sklearn.datasets import make_moons
+from matplotlib.image import imread
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+image = imread('island.png')
+
+plt.imshow(image)
+
+X = image.reshape(-1, 4)
+kmeans = KMeans(n_clusters=3).fit(X)
+segmented_img = kmeans.cluster_centers_[kmeans.labels_]
+segmented_img = segmented_img.reshape(image.shape)
+
+plt.imshow(segmented_img)
+
+b = kmeans.cluster_centers_[[1,2,0,1,1]]
